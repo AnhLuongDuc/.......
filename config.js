795e51dcee578529cfc0f9990341f35af55acdfd
AnhLuongDuc <96977439+AnhLuongDuc@users.noreply.github.com>
2022-01-02 17:46:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,15 +6,14 @@ const CONFIG = {
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    title: 'Chị có yêu em hong 🥰',
+    desc: 'Chị yêu em đúng hem ',
+    btnYes: 'Yêu lắm <33',
+    btnNo: 'Không yêu',
+    question: 'Tại sao chị yêu em dạ',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    reply: 'Yêu thì yêu thui',
+    mess: 'Em biết mà 🥰. Yêu chị nhiều nhiều 😘😘',
+    btnAccept: 'Yêu emmmm <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
